faf0a90d2cdf1b7f02bb32f5ad49e6cbac4f973c
StilyanI <stilyan.i.ivanov@gmail.com>
2023-05-18 11:44:11 +0300

    Alarm class added

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import PySimpleGUI as sg
 
+
+class Alarm:
+    def __init__(self, time, active, repeat):
+        self.time = time
+        self.active = active
+        self.repeat = repeat
+
+
+number_of_alarms = 0
+alarms = []
+
 alarms_list = [
     [sg.Text("Alarms")],
-    [sg.Listbox(values=[],enable_events=True, size=(40,20))]
+    [sg.Listbox(values=[], enable_events=True, size=(40, 20))]
+]
+
+alarm_settings = [
+    [sg.In(size=(40, 1), key="INPUT")],
+    [sg.Button("Add")]
 ]
 
 layout = [
     [
         sg.Column(alarms_list),
         sg.VSeparator(),
+        sg.Column(alarm_settings),
         sg.Button("Exit")
     ]
 ]
 
-window = sg.Window("Program",layout)
+window = sg.Window("Program", layout)
 
 while True:
     event, values = window.read()
