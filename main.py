c41630b275af94ec29e407e97557c45f2782daf3
StilyanI <103041263+StilyanI@users.noreply.github.com>
2023-06-07 23:42:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import PySimpleGUI as sg
+import os
+import time
+import threading
 
 
 class Alarm:
-    def __init__(self, time, active, repeat):
-        self.time = time
+    def __init__(self, hour, minute, active, repeat):
+        self.hour = hour
+        self.minute = minute
         self.active = active
         self.repeat = repeat
 
 
-number_of_alarms = 0
 alarms = []
 
 alarms_list = [
     [sg.Text("Alarms")],
-    [sg.Listbox(values=[], enable_events=True, size=(40, 20))]
+    [sg.Listbox(alarms, enable_events=True, size=(40, 20), key="LIST")]
 ]
 
 alarm_settings = [
@@ -31,16 +34,41 @@ layout = [
     ]
 ]
 
+settings = sg.UserSettings(filename="settings.ini", path=os.getcwd(), use_config_file=True)
+sg.theme(settings["GUI"]["theme"])
 window = sg.Window("Program", layout)
+
+
+def check_time():
+    currTime = time.strftime("%H:%M")
+    tmpTime = currTime
+    while not stopFlag:
+        if currTime != tmpTime:
+            print(currTime)
+            tmpTime = currTime
+            for i in alarms:
+                if i.hour + ":" + i.minute == tmpTime:
+                    # smeni tva na funkciq za alrmata
+                    print("Evala bratle")
+        currTime = time.strftime("%H:%M")
+        time.sleep(1)
+
+
+thread = threading.Thread(target=check_time)
+stopFlag = False
+thread.start()
 
 while True:
     event, values = window.read()
     if event == "Add":
-        number_of_alarms += 1
-        alarms.append(Alarm(values["IN_H"] + ":" + values["IN_M"], True, values["REPEAT"]))
-        # print(values["IN_H"] + ":" + values["IN_M"])
-        print(alarms[0].repeat)
+        alarms.append(Alarm(values["IN_H"], values["IN_M"], True, values["REPEAT"]))
+        list = []
+        for i in alarms:
+            list.append(i.hour + ":" + i.minute)
+        window["LIST"].update(list)
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
 
+stopFlag = True
+thread.join()
 window.close()
